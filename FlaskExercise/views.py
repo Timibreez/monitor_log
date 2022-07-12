a5a9bfcd0bd673edf7590de19f5579cf831c14cb
Timibreez <ffftimi@gmail.com>
2022-07-12 15:48:05 +0100

    set Log details in views

--- FlaskExercise/views.py
+++ FlaskExercise/views.py
@@ -7,6 +7,15 @@ def home():
     log = request.values.get('log_button')
     # TODO: Appropriately log the different button presses
     #   with the appropriate log level.
+    if log:
+        if log == 'info':
+            app.logger.info('No errors')
+        elif log == 'warning':
+            app.logger.warning('warning occured')
+        elif log == 'error':
+            app.logger.error('an error occured')
+        elif log == 'critical':
+            app.logger.critical('critical error')
     return render_template(
         'index.html',
         log=log
